3b5774b772d92933c8126b34cef2c44e29eee2a6
C Amox <code@amox.mx>
2016-01-08 21:47:59 -0700

    Sublime package creation
    Shell script, test package with current source files.

---
+++ build/create_sublime_package.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PACKAGE_LOCATION="../../../../downloads/Sublime/Oracle.sublime-package"
+echo "Generating zipped Sublime Text package to downloads/Sublime/Oracle.sublime-package"
+# For the zip archive to maintain the structure
+cd ../src/Sublime/Packages/Oracle
+rm ${PACKAGE_LOCATION}
+# Exclude JSON files used for tmlanguage XML files generation
+zip -r ${PACKAGE_LOCATION} . -x *JSON*
