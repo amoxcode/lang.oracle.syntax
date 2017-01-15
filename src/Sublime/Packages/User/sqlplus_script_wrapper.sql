67cafabbe82a2a002efd3150d161b717f56d6fef
Cuauhtemoc Amox <code@amox.mx>
2017-01-14 22:13:53 -0700

    Build System - SQL*Plus, confirm execution
    Add message to wrapper script confirm script execution when choosing a
    variant

--- src/Sublime/Packages/User/sqlplus_script_wrapper.sql
+++ src/Sublime/Packages/User/sqlplus_script_wrapper.sql
@@ -3,12 +3,10 @@ set feedback off
 set lines 120
 set long 2000
 set longc 2000
-set pause off
 set serveroutput on size unlimited
 set tab off
 --set term off
 set time on
-set timing on
 set verify off
 prompt &_O_VERSION
 prompt
@@ -16,8 +14,11 @@ prompt ..
 prompt .. Executing script &2
 prompt .. Connected as &_USER.@&_CONNECT_IDENTIFIER
 prompt ..
- 
-prompt
+prompt .. Press [ENTER] to proceed, [CTRL] + C to abort.
+prompt .. When finished, type exit and press [ENTER] to return to Sublime Text
+prompt ..
+pause
+clear scr
 @&1
  
 rollback
